f99ccd2755c448433a97b5067bdf235443fad9da
Igor.K <igor.k.nsk@gmail.com>
2023-12-15 04:01:12 +0700

    dsad

--- src/data/carsSettingsMockData.js
+++ src/data/carsSettingsMockData.js
@@ -127,12 +127,12 @@ export const mockData = {
   ],
   users: [
     {
-      id: '1',
+      user_id: '1',
       email: 'kenherli@gmail.com',
       role: 'admin'
     },
     {
-      id: '2',
+      user_id: '2',
       email: 'petrov@mail.pl',
       role: 'user'
     },
